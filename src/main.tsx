e37c4e427e208d1dc4903ca97f17c8338779ab03
Higa <codehiga@gmail.com>
2023-01-12 02:23:41 -0300

    feat: remoção de dev mode

---
+++ src/main.tsx
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom/client'
+import { HomePage } from './components/pages/home-page'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<HomePage />)
