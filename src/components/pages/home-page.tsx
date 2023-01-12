115e0042a6e980e0584e025411122dc15f00bfad
higa <codehiga@gmail.com>
2023-01-12 11:19:18 -0300

    feat: botao signout adicionado

--- src/components/pages/home-page.tsx
+++ src/components/pages/home-page.tsx
@@ -1,7 +1,20 @@
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useLocation } from "../../hooks/useLocation";
+
 export const HomePage = () => {
+  const { signOut } = useContext(AuthContext);
+  const { go } = useLocation();
+
+  async function handleSignOut() {
+    await signOut();
+    go("/login");
+  }
+  
   return(
     <div>
       <div>Página inicial</div>
+      <button onClick={handleSignOut}>Fazer logout!</button>
     </div>
   )
 }
