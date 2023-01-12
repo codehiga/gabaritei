63dd3f4c76732be7da4b590c334733fc94601900
Higa <codehiga@gmail.com>
2023-01-12 09:58:54 -0300

    feat: configuração do tailwind no app

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import { AppRoutes } from './routes/app-routes'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<AppRoutes />)
