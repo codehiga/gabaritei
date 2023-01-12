56a75ed7b80c2836c752f034a13c1880a976fa05
Higa <codehiga@gmail.com>
2023-01-12 02:36:56 -0300

    feat: stringify e parse nos values

--- src/services/local-storage.ts
+++ src/services/local-storage.ts
@@ -2,11 +2,13 @@ export class LocalStorage {
   private localStorageRepository = localStorage;
 
   save(key: string, value: any) {
+    value = JSON.stringify(value);
     this.localStorageRepository.setItem(key, value);
   }
 
   get(key: string) {
-    this.localStorageRepository.getItem(key);
+    const data = this.localStorageRepository.getItem(key);
+    return data && JSON.parse(data);
   }
 
   delete(key: string) {
