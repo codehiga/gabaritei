c108753f351d5c0c38116d223a7800932092ca24
Higa <codehiga@gmail.com>
2023-01-12 02:22:58 -0300

    feat: implementação de contrato do repositorio com fb

---
+++ src/services/api.ts
@@ -0,0 +1,10 @@
+import { addDoc, collection } from "firebase/firestore";
+import { Gabarito } from "../interfaces/gabarito";
+import { db } from "../libs/firebase";
+import { GabaritoRepository } from "../repositories/gabarito-repository";
+
+export class FirebaseGabaritoRepository implements GabaritoRepository {
+  async salvar(data: Gabarito): Promise<void> {
+    await addDoc(collection(db, "gabaritos"), data);
+  }
+}
